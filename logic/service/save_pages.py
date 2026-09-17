e2536d4986e81f0ba8c4c623982d35795965affe
donneko <117331095+donneko@users.noreply.github.com>
2026-09-17 11:00:44 +0900

    change

--- logic/service/save_pages.py
+++ logic/service/save_pages.py
@@ -1,10 +1,39 @@
-def savePages(pages,outputConfig):
 
+def saveSomePage(pages,setting):
+
+    pagesLength = len(pages)
+    digits = len(str(pagesLength))
+
+    for i in range(pagesLength):
+
+        fileNumber = "" if pagesLength > 1 else f"_{str(i).zfill(digits)}"
+
+        page = pages[i]
+        page.save(
+            f"{setting["path"]}{fileNumber}.{setting["format"].lower()}",
+            setting["format"],
+            resolution=setting["dpi"]
+        )
+
+def saveOnePage(pages,setting):
 
     pages[0].save(
-        f"{outputConfig["directory"]}/out-image.{outputConfig["format"]}",
-        outputConfig["format"],
+        f"{setting["path"]}.{setting["format"].lower()}",
+        setting["format"],
         save_all=True,
         append_images=pages[1:],
-        resolution=300
+        resolution=setting["dpi"]
     )
+
+def savePages(pages,outputConfig):
+
+    setting = {
+        "path":f"{outputConfig["directory"]}/{outputConfig["name_template"]}",
+        "format":outputConfig["format"],
+        "dpi":outputConfig["dpi"]
+    }
+
+    if outputConfig["save_one_page"].lower() == "true":
+        saveOnePage(pages,setting)
+    else:
+        saveSomePage(pages,setting)
